b57dbad4fd1244859d403ba94e0639868ce1c450
Susan Lee <seungeun92@gmail.com>
2017-11-03 20:31:45 -0400

    first

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'C1GP0myyTkTaUXrNbdu92hmJa'
+consumer_secret = 'hKI3VzAnucjoSf6fs6wV1NshbAlvo50PFanqy7bWZx8jvmIMsm'
+access_token = '469913887-I2GlJzStsKsmyU3mejjU5g7qu2oM5RgHffPRyTcJ'
+access_token_secret = 'lvUKAf9majsS0hsuL8PWN9e5NDCqbuX1oORFLmBbhnIGn'
